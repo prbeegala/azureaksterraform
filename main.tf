9fcf077b972eb3bc92f0aa931e5b933f03a31d02
Prakash Beegala <prbeegal@microsoft.com>
2018-09-25 11:29:03 +0100

    added the kubernettes config

--- main.tf
+++ main.tf
@@ -1,18 +1,42 @@
 provider "azurerm" {
-  subscription_id = "XXXXXX"
-  tenant_id       = "XXXXXX"
+  subscription_id = "52eb3891-6f09-4350-8707-4dc848cba302"
+  tenant_id       = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+  version = "~>1.5"
 }
 
-resource "azurerm_resource_group" "rg01" {
-  name     = "rg.aks.terraform"
-  location = "westeurope"
+resource "azurerm_resource_group" "k8s" {
+    name     = "${var.resource_group_name}"
+    location = "${var.location}"
 }
 
-resource "azurerm_storage_account" "sa01" {
-  access_tier              = "Hot"
-  account_replication_type = "LRS"
-  account_tier             = "Standard"
-  location                 = "westeurope"
-  resource_group_name      = "${azurerm_resource_group.rg01.name}"
-  name                     = "saaksterraform"
+resource "azurerm_kubernetes_cluster" "k8s" {
+    name                = "${var.cluster_name}"
+    location            = "${azurerm_resource_group.k8s.location}"
+    resource_group_name = "${azurerm_resource_group.k8s.name}"
+    dns_prefix          = "${var.dns_prefix}"
+
+    linux_profile {
+        admin_username = "ubuntu"
+
+        ssh_key {
+        key_data = "${file("${var.ssh_public_key}")}"
+        }
+    }
+
+    agent_pool_profile {
+        name            = "default"
+        count           = "${var.agent_count}"
+        vm_size         = "Standard_DS2_v2"
+        os_type         = "Linux"
+        os_disk_size_gb = 30
+    }
+
+    service_principal {
+        client_id     = "${var.client_id}"
+        client_secret = "${var.client_secret}"
+    }
+
+    tags {
+        Environment = "Development"
+    }
 }
